5134e5204f088a449173f048eddbf44423cebd03
Andres Moreno <andres@andmore.dev>
2025-05-01 14:29:35 -0600

    update event api mjs

--- src/backend/appsync-handlers/event-api.mjs
+++ src/backend/appsync-handlers/event-api.mjs
@@ -3,19 +3,35 @@ import { util } from '@aws-appsync/utils'
 
 const TABLE = 'appsync-events-app-DynamoDBTableName-NBAUU4K2D9NY'
 
-export const onPublish = {
-  request(ctx) {
-    const channel = ctx.info.channel.path
-    const timestamp = util.time.nowISO8601()
-    return ddb.batchPut({
-      tables: {
-        [TABLE]: ctx.events.map(({id, payload}) => ({
-          channel, id, timestamp, ...payload,
-        })),
-      },
-    })
+export const onSubscribe = {
+  request(event) {
+    if (event.info.channel.segments.length > 2) {
+      util.error('Invalid Room Name - To Many Segments');
+    }
+
+    if(event.info.channel.segments.includes('*')) {
+      util.error('Invalid Room Name - Wildcards');
+    }
+
+    const channel = event.info.channel.path
+    const room = event.info.channel.segments[1]
+    const timestamp = util.time;
+
+    ddb.put({
+      key: { id: room },
+      item: {
+        channel,
+        id: room,
+        timestamp
+      }
+    });
   },
   response(ctx) {
-    return ctx.result.data[TABLE].map(({ id, ...payload }) => ({ id, payload }))
-  },
+    const { error, result } = ctx;
+    if (error) {
+      return util.appendError(error.message, error.type, result);
+    }
+
+    return 'Successfully subscribed to channel';
+  }
 }
