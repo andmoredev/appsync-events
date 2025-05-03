f4e0be683caa69dace4e002f456824b28a093592
Andres Moreno <andres@andmore.dev>
2025-05-03 08:37:55 -0600

    try to use include

--- src/backend/appsync-handlers/event-api.mjs
+++ src/backend/appsync-handlers/event-api.mjs
@@ -2,17 +2,18 @@ import * as ddb from '@aws-appsync/utils/dynamodb'
 import { util } from '@aws-appsync/utils'
 
 export const onSubscribe = {
-  request(ctx) {
-        if (ctx.info.channel.segments.length > 2) {
+  request(event) {
+        // Did I Get it??
+        if (event.info.channel.segments.length > 2) {
           util.error('Invalid Room Name - To Many Segments');
         }
 
-        if(ctx.info.channel.segments.includes('*')) {
+        if(event.info.channel.segments.includes('*')) {
           util.error('Invalid Room Name - Wildcards');
         }
 
-        const channel = ctx.info.channel.path
-        const room = ctx.info.channel.segments[1]
+        const channel = event.info.channel.path
+        const room = event.info.channel.segments[1]
         const timestamp = util.time.nowEpochMilliSeconds();
 
         const id = util.autoId();
