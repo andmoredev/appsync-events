c1ce82626e1cdf37c07ed88d084ae85f7119df89
Andres Moreno <andres@andmore.dev>
2025-05-03 09:20:14 -0600

    get the file back to 2 space indentation

--- src/backend/appsync-handlers/event-api.mjs
+++ src/backend/appsync-handlers/event-api.mjs
@@ -3,30 +3,30 @@ import { util } from '@aws-appsync/utils'
 
 export const onSubscribe = {
   request(event) {
-        // Did I Get it?? Am I going to cause an updated?
-        if (event.info.channel.segments.length > 2) {
-          util.error('Invalid Room Name - To Many Segments');
-        }
-
-        if(event.info.channel.segments.includes('*')) {
-          util.error('Invalid Room Name - Wildcards');
-        }
-
-        const channel = event.info.channel.path
-        const room = event.info.channel.segments[1]
-        const timestamp = util.time.nowEpochMilliSeconds();
-
-        const id = util.autoId();
-        return ddb.put({
-            key: { id: room },
-            item: {
-                id: room,
-                channel,
-                timestamp
-            }
-        });
-    },
-    response(ctx) {
-        return ctx.result;
+    // Did I Get it?? Am I going to cause an updated?
+    if (event.info.channel.segments.length > 2) {
+      util.error('Invalid Room Name - To Many Segments');
     }
+
+    if (event.info.channel.segments.includes('*')) {
+      util.error('Invalid Room Name - Wildcards');
+    }
+
+    const channel = event.info.channel.path
+    const room = event.info.channel.segments[1]
+    const timestamp = util.time.nowEpochMilliSeconds();
+
+    const id = util.autoId();
+    return ddb.put({
+      key: { id: room },
+      item: {
+        id: room,
+        channel,
+        timestamp
+      }
+    });
+  },
+  response(ctx) {
+    return ctx.result;
+  }
 }
