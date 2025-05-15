e7d11b98639b731f215ac4f5e95e7a3b5dd01237
Andres Moreno <andres@andmore.dev>
2025-05-15 07:37:20 -0600

    slim down the function

--- src/backend/functions/without-powertools/index.mjs
+++ src/backend/functions/without-powertools/index.mjs
@@ -1,13 +1,6 @@
 import { initializePowertools, logger } from '../shared/lambda-powertools.mjs';
 
 export const handler = initializePowertools(async (event, context) => {
-  logger.info('Event: ', event);
-  logger.info('Context: ', context);
-
-  const response = {
-    processed: true,
-    original_payload: event
-  };
-
-  return [response, response];
+  logger.info('Event:', event);
+  return event.events;
 });
